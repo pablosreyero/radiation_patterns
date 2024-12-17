1cf97bb0d12f25b0a10130da44405dff38e6389a
pablosreyero <pablosreyero@gmail.com>
2024-12-17 14:22:18 +0100

    Code finished, just an error to go

--- plot_3D_from_MSI.py
+++ plot_3D_from_MSI.py
@@ -3,6 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import tensorflow as tf
+from scipy.interpolate import interp1d
+
+# sionna imports
+from sionna.constants import PI
+from sionna.rt.antenna import polarization_model_2, polarization_model_1
+from sionna.rt.antenna import Antenna, visualize
+
 
 def msi_file_reader(msi_path):
     """
@@ -65,7 +72,7 @@ def msi_file_reader(msi_path):
     return antenna_gain, horizontal_gains, vertical_gains
 
 
-def cross_weighted_algorithm(antenna_gain, horizontal_gains, vertical_gains):
+def cross_weighted_algorithm(gain_theta, gain_phi):
     '''
     This function takes as input both horizontal and vertical converted
     angles as well as their associated gains. The aim is to reconstruct in
@@ -80,55 +87,65 @@ def cross_weighted_algorithm(antenna_gain, horizontal_gains, vertical_gains):
         vertical plane of the antenna.
 
     Outputs:
-    - 
+    - G_w_theta_phi () -> Final gains of the interpolated radiation pattern
     '''
 
-    # Convert gains from dB to linear scale
-    hor_gains_linear = 10 ** (horizontal_gains / 10)
-    ver_gains_linear = 10 ** (vertical_gains / 10)
+    # Convert to dBs to obtain -> G_H(phi) & G_V(theta)
+    gain_theta_dB_n = 10.0 * tf.math.log(gain_theta) / tf.math.log(10.0)
+    gain_phi_dB_n = 10.0 * tf.math.log(gain_phi) / tf.math.log(10.0)
 
-    # Initialize a matrix for storing gain values
-    G_w_theta_phi = np.zeros((len(horizontal_angles), len(vertical_angles)))
-    print(f"Horizontal gains: {horizontal_gains}")
-    print(f"Vertical gains: {vertical_gains}")
+    # compute weights (recall that here we must use linear units)
+    w1 = gain_theta * (1 - gain_phi)
+    w2 = gain_phi * (1 - gain_theta)
 
-    # constant to check the first values of both vertical and horizontal planes
-    a = 0
+    # define the normalization parameter (k=2 for best results)
+    k=2
 
-    # Loop through horizontal and vertical angles
-    for i, horizontal_gain in enumerate(horizontal_angles):
-        for j, vertical_gain in enumerate(vertical_angles):
+    # compute the final gain
+    G_w_theta_phi = gain_phi_dB_n * (w1/tf.pow(tf.pow(w1,k)+tf.pow(w2,k),1/k)) + gain_theta_dB_n * (w2/tf.pow(tf.pow(w1,k)+tf.pow(w2,k),1/k))
 
-            if a <= 3:
-                print(f"These are the gain values for the HORIZONTAL plane: {horizontal_gain}")
-                print(f"These are the gain values for the VERTICAL plane: {vertical_gain}")
-                a += 1
-
-            # Normalize linear gains
-            g_h_phi_i = (hor_gains_linear[i] - np.min(hor_gains_linear)) / (np.max(hor_gains_linear) - np.min(hor_gains_linear))
-            g_v_theta_i = (ver_gains_linear[j] - np.min(ver_gains_linear)) / (np.max(ver_gains_linear) - np.min(ver_gains_linear))
-
-            # check that both values are between 0 and 1
-            if (g_h_phi_i>1 or g_h_phi_i<0) and (g_v_theta_i>1 or g_v_theta_i<0):
-                print('One of the normalized values is not inside (0, ..., 1)')
-                exit()
-
-            # Compute weight functions
-            w1_theta_phi_i = g_v_theta_i * (1 - g_h_phi_i)
-            w2_theta_phi_i = g_h_phi_i * (1 - g_v_theta_i)
-
-            # Convert normalized linear gains back to dB
-            G_h_phi_dB_i = 10 * np.log10(g_h_phi_i)
-            G_v_theta_dB_i = 10 * np.log10(g_v_theta_i)
-
-            # Compute weighted antenna gain
-            k = 2  # Normalization parameter
-            G_w_theta_phi[i, j] = (G_h_phi_dB_i) * w1_theta_phi_i/((w1_theta_phi_i**k + w2_theta_phi_i**k)**(1/k)) + (G_v_theta_dB_i) * w2_theta_phi_i / (
-                (w1_theta_phi_i**k + w2_theta_phi_i**k)**(1/k)
-            )
-    print(f"These are the values for i and j: {i}, {j}")
-    print(f"This is the shape of G_w_theta_phi: {np.shape(G_w_theta_phi)}")
     return G_w_theta_phi
+
+
+# Custom antenna pattern function to be used in Sionna
+def custom_pattern(theta, phi, slant_angle=0.0, polarization_model=2, dtype=tf.complex64):
+
+    rdtype = dtype.real_dtype
+    k_dB = antenna_gain  # IMPORTANT: the gain should be in dBi not in dBd (previous conversion has been done)
+    #k_dB = 0    # Uncomment if you want to normalize the radiattion pattern
+    k_dB = tf.cast(k_dB,rdtype)    
+    k = 10**(k_dB/10)    
+    theta = tf.cast(theta, rdtype)
+    phi = tf.cast(phi, rdtype)
+    slant_angle = tf.cast(slant_angle, rdtype) # This angle is associate with the polarization
+
+    # Common for SIONNA: (IMPORTANT) same shape does not mean same intervals: (0,pi) and (-pi,pi)
+    if not theta.shape== phi.shape: 
+        raise ValueError("theta and phi must have the same shape.")
+    if polarization_model not in [1,2]:
+        raise ValueError("polarization_model must be 1 or 2")
+
+    #Use the interpolator to create a function for each plane
+    gain_theta = tf.py_function(func=interp_v, inp=[theta], Tout=tf.float32)
+    gain_phi = tf.py_function(func=interp_h, inp=[phi], Tout=tf.float32)
+
+    # Estimation of the 3D pattern using the cross weighted algorithm
+    gain_theta_phi_dB_n = cross_weighted_algorithm(gain_theta, gain_phi)
+
+    #Converting from dBn to linear
+    g_theta_phi = tf.pow(10.0, gain_theta_phi_dB_n / 10.0) #normalized gain for each per (theta, phi) 
+    
+    # The values from the MSI files are gain/attenuation values (power)
+    # We need to convert from power to field components F = sqrt(P)    
+    temp = tf.sqrt(k)*tf.sqrt(g_theta_phi)             
+    c = tf.complex(temp, tf.zeros_like(temp))
+
+    # Common step in sionna, this functions obtain the Field components based on the slant angle
+    if polarization_model==1:
+        return polarization_model_1(c, theta, phi, slant_angle)
+    else:
+        return polarization_model_2(c, slant_angle)
+
 
 if __name__ == '__main__':
     # Load the data from the file
@@ -141,8 +158,43 @@ if __name__ == '__main__':
     print(f'These are the VERTICAL GAINS of the antenna: {vertical_gains}')
 
     # only select gains associated to angles liying on the positive side of the x plane, i.e., 1st and 4th quadrant
-    
+    temp1 = vertical_gains[0:91][::-1] # sort it out in an inverse manner
+    temp2 = vertical_gains[270:360][::-1]
+    filtered_vertical_gains = np.concatenate((temp1, temp2))
 
+    # Recall that the .msi file contains attenuations in dB, so the real gain is: G(angles) = Gain - .msi(angles)
+    g_hor_dB = antenna_gain - horizontal_gains
+    g_ver_dB = antenna_gain - filtered_vertical_gains
+
+    # The normalized values in dB will be -- .msi(angles)
+    g_hor_dB_n = - horizontal_gains
+    g_ver_dB_n = - filtered_vertical_gains 
+
+    #normalized gain from dB to lineal. Normalized values have been utilized, the gain factor will be added at the end.
+    gain_v = 10**(g_ver_dB_n/10)
+    gain_h_temp = 10**(g_hor_dB_n/10) #A temporal variable has been used because we are going to shift the angles to [-pi,pi)
+
+    theta = np.linspace(0, 2*PI, 360)
+
+    # Shift the theta values to range from -pi to pi
+    theta_shifted = np.where(theta > PI, theta - 2*PI, theta)
+    sorted_indices = np.argsort(theta_shifted)
+    gain_h = gain_h_temp[sorted_indices]
+
+    # Once angles and gain in the MSI file has been parse to be aligned with the spherical coordinate 
+    # we can work with the variables (theta,phi).
+    theta_rad = np.linspace(0, PI, 181)  # Zenith angles
+    phi_rad = np.linspace(-PI, PI, 360)  # Azimuth angles
+
+    # Interpolation of each plane: It is required because we only have data with a resolution of one degree from the MSI file
+    interp_v = interp1d(theta_rad, gain_v, kind='linear', fill_value="extrapolate")
+    interp_h = interp1d(phi_rad, gain_h, kind='linear', fill_value="extrapolate")
+
+    # create the antenna object
+    antenna = Antenna(pattern=custom_pattern, polarization="H", dtype=tf.complex64)
+
+    # visualize the antenna pattern
+    fig_v, fig_h, fig_3d = visualize(custom_pattern)
 
     '''# Call the cross-weighted algorithm
     weighted_antenna_gains = cross_weighted_algorithm(
